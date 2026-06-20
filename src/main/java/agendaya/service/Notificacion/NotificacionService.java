b07f5c1c98d5bb325dc47492b861e63e345920d4
Lucia Varano <lucia.varano19@gmail.com>
2026-06-20 12:31:11 -0300

    Implemento pruebas unitarias US01, US02 y US03 del módulo de notificaciones

---
+++ src/main/java/agendaya/service/Notificacion/NotificacionService.java
@@ -0,0 +1,21 @@
+package agendaya.service.Notificacion;
+
+public class NotificacionService {
+    
+    public boolean enviarConfirmacionProfesional(String email){
+
+        return true;
+    }
+
+
+    public boolean enviarConfirmacionCliente(String email){
+
+        return true;
+    }
+
+
+    public boolean notificarCancelacionProfesional(String email){
+
+        return true;
+    }
+}
